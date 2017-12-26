0750f35311f103edcdc49c8faf2f2bc134f0c4cb
Jesús Burgos <jesus@visrez.com>
2017-12-26 09:20:19 +0100

    Adds a script to download inference graphs

---
+++ download_graphs.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+url_prefix = "https://s3.amazonaws.com/bibo.inference/20171224/"
+files = {
+    "number_recognition_frozen_inference_graph.pb":
+        "data/attention_ocr/number_recognition_frozen_inference_graph.pb",
+    "bib_detection_frozen_inference_graph.pb":
+        "data/object_detection/bib_detection_frozen_inference_graph.pb"
+}
+
+for url, path in files.items():
+    url = f"{url_prefix}{url}"
+    print(url)
+    urllib.request.urlretrieve(url, path)
